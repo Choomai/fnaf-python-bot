1a6cb3bc85adbef0d46f233db21be2aa6b81b120
The2AndOnly <no email>
2026-02-28 15:54:23 -0500

    fix modified venv

--- beatfnaf1.py
+++ beatfnaf1.py
@@ -40,6 +40,32 @@ coordinates = {
     "star3" : (0.27291666666666664, 0.4703703703703704)
 }
 
+# Monkey patch for pyautogui's "pixelMatchesColor" function
+def customPMC(x=0, y=0, expectedRGBColor=(0, 0, 0), tolerance=0, sample=None):
+    if isinstance(x, pg.collections.abc.Sequence) and len(x) == 2:
+        raise TypeError('pixelMatchesColor() has updated and no longer accepts a tuple of (x, y) values for the first argument. Pass these arguments as two separate arguments instead: pixelMatchesColor(x, y, rgb) instead of pixelMatchesColor((x, y), rgb)')
+
+    pix = pg.pixel(x, y) if sample == None else sample
+    if len(pix) == 3 or len(expectedRGBColor) == 3:  # RGB mode
+        r, g, b = pix[:3]
+        exR, exG, exB = expectedRGBColor[:3]
+        return (abs(r - exR) <= tolerance) and (abs(g - exG) <= tolerance) and (abs(b - exB) <= tolerance)
+    elif len(pix) == 4 and len(expectedRGBColor) == 4:  # RGBA mode
+        r, g, b, a = pix
+        exR, exG, exB, exA = expectedRGBColor
+        return (
+            (abs(r - exR) <= tolerance)
+            and (abs(g - exG) <= tolerance)
+            and (abs(b - exB) <= tolerance)
+            and (abs(a - exA) <= tolerance)
+        )
+    else:
+        assert False, (
+            'Color mode was expected to be length 3 (RGB) or 4 (RGBA), but pixel is length %s and expectedRGBColor is length %s'  # noqa
+            % (len(pix), len(expectedRGBColor))
+        )
+pg.pixelMatchesColor = customPMC
+
 def onPress(key):
     try:
         match key.char:
